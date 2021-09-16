8993f9544f0d821335f178be657b3acfe8615b68
Pioro <89617676+pityofromance@users.noreply.github.com>
2021-09-16 19:40:35 +0900

    Create Ex01.hello.java
    ex01 upload / added folder on github webpage

---
+++ java/java01_basic/Ex01.hello.java
@@ -0,0 +1,13 @@
+package java01_basic;
+
+public class Ex01_hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("~~ Hello Java !!! ~~");
+		System.out.println("안녕하세요");
+		System.out.println("헬로 월드~");
+		
+	} //main
+
+} //class
